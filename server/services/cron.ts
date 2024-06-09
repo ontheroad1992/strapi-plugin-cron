33cf6724386cf26358a0b953c9a114ae8cb79f0a
kenthe <kenthe@tencent.com>
2024-06-09 14:12:45 +0800

    feat: 增加 intervals 返回值

--- server/services/cron.ts
+++ server/services/cron.ts
@@ -1,24 +1,22 @@
-import { CronJob, CronJobInputData } from '../../types'
-import { pluginName } from '../../utils/plugin'
-import { captureConsoleOutput } from '../utils'
-import { getDefaultModuleExport } from '../utils/extensions'
-import { CronJobSchema } from './cron-job/schema'
+import { CronJob, CronJobInputData } from '../../types';
+import { pluginName } from '../../utils/plugin';
+import { captureConsoleOutput } from '../utils';
+import { getDefaultModuleExport, parseScheduleToSeconds } from '../utils/extensions';
+import { CronJobSchema } from './cron-job/schema';
 
-import { Strapi } from '@strapi/strapi'
-import { scheduleJob, scheduledJobs } from 'node-schedule'
+import { Strapi } from '@strapi/strapi';
+import { scheduleJob, scheduledJobs } from 'node-schedule';
 
 export default ({ strapi }: { strapi: Strapi }) => ({
   initialize: async function () {
-    const published = await strapi
-      .service(`plugin::${pluginName}.cron-job`)
-      .getPublished()
-    for (const cronJob of published) this.update(cronJob)
+    const published = await strapi.service(`plugin::${pluginName}.cron-job`).getPublished();
+    for (const cronJob of published) this.update(cronJob);
   },
 
   update: async function (cronJob: CronJob) {
-    this.cancel(cronJob)
-    const isNotPublished = !cronJob.publishedAt
-    if (isNotPublished) return
+    this.cancel(cronJob);
+    const isNotPublished = !cronJob.publishedAt;
+    if (isNotPublished) return;
     scheduleJob(
       cronJob.name,
       {
@@ -27,70 +25,65 @@ export default ({ strapi }: { strapi: Strapi }) => ({
         rule: cronJob.schedule,
       },
       await createCronJobCallback(cronJob)
-    )
+    );
   },
 
   validateData: (data: CronJobInputData) => {
-    const omitProps = {}
-    data.executeScriptFromFile
-      ? (omitProps['script'] = true)
-      : (omitProps['pathToScript'] = true)
+    const omitProps = {};
+    data.executeScriptFromFile ? (omitProps['script'] = true) : (omitProps['pathToScript'] = true);
 
-    const validation = CronJobSchema.omit(omitProps).safeParse(data)
+    const validation = CronJobSchema.omit(omitProps).safeParse(data);
     if (!validation.success) {
-      return { errors: validation['error'].issues }
+      return { errors: validation['error'].issues };
     }
-    return { errors: null }
+    return { errors: null };
   },
 
   cancel: async (cronJob: CronJob) => {
-    scheduledJobs[cronJob.name]?.cancel()
+    scheduledJobs[cronJob.name]?.cancel();
   },
 
   trigger: async (cronJob: CronJob) => {
     const cronJobCallback = await createCronJobCallback(cronJob, {
       dryRun: true,
-    })
-    cronJobCallback()
+    });
+    cronJobCallback();
   },
-})
+});
 
-const createCronJobCallback = async (
-  cronJob: CronJob,
-  { dryRun = false } = {}
-) => {
-  let { iterationsLimit, iterationsCount } = cronJob
-  const hasLimitedIterations = iterationsLimit > -1
-  const script = await getCronJobScript(cronJob)
+const createCronJobCallback = async (cronJob: CronJob, { dryRun = false } = {}) => {
+  let { iterationsLimit, iterationsCount } = cronJob;
+  const hasLimitedIterations = iterationsLimit > -1;
+  const script = await getCronJobScript(cronJob);
 
   return function callback() {
     if (!dryRun && hasLimitedIterations && iterationsCount >= iterationsLimit) {
-      scheduledJobs[cronJob.name].cancel()
-      return
+      scheduledJobs[cronJob.name].cancel();
+      return;
     }
 
     captureConsoleOutput(async () => {
-      console.log(`[${new Date().toLocaleString()}]`)
+      console.log(`[${new Date().toLocaleString()}]`);
       try {
-        await script({ strapi, cronJob })
+        const intervals = parseScheduleToSeconds(cronJob.schedule);
+        await script({ strapi, cronJob, intervals });
       } catch (e) {
-        console.log(e)
+        console.log(e);
       }
     }).then((logs) => {
-      const data: Partial<CronJob> = { latestExecutionLog: logs }
-      if (!dryRun && hasLimitedIterations)
-        data.iterationsCount = ++iterationsCount
-      strapi.service(`plugin::${pluginName}.cron-job`).update(cronJob.id, data)
-    })
-  }
-}
+      const data: Partial<CronJob> = { latestExecutionLog: logs };
+      if (!dryRun && hasLimitedIterations) data.iterationsCount = ++iterationsCount;
+      strapi.service(`plugin::${pluginName}.cron-job`).update(cronJob.id, data);
+    });
+  };
+};
 
 const getCronJobScript = async (
   cronJob: CronJob
 ): Promise<
-  ({ strapi, cronJob }: { strapi: Strapi; cronJob: CronJob }) => Promise<void>
+  ({ strapi, cronJob, intervals }: { strapi: Strapi; cronJob: CronJob; intervals: number }) => Promise<void>
 > => {
   return cronJob.executeScriptFromFile
     ? await getDefaultModuleExport(cronJob.pathToScript)
-    : new Function(cronJob.script)
-}
+    : new Function(cronJob.script);
+};
--- server/utils/extensions.ts
+++ server/utils/extensions.ts
@@ -1,9 +1,22 @@
-import { pluginName } from '../../utils/plugin'
+import BigNumber from 'bignumber.js';
 
-const extensionsDirPath = `${process.cwd()}/src/extensions/${pluginName}`
+import { pluginName } from '../../utils/plugin';
 
-const getFilePath = (path: string) => `${extensionsDirPath}${path}`
+const extensionsDirPath = `${process.cwd()}/src/extensions/${pluginName}`;
+
+const getFilePath = (path: string) => `${extensionsDirPath}${path}`;
 
 export const getDefaultModuleExport = async (path: string) => {
-  return (await import(getFilePath(path))).default
+  return (await import(getFilePath(path))).default;
+};
+
+export function parseScheduleToSeconds(schedule: string) {
+  const [seconds, minutes, hours, day] = schedule.split(' ').map((item) => (item !== '*' ? item : 0));
+
+  const allSeconds = new BigNumber(seconds)
+    .plus(new BigNumber(minutes).times(60))
+    .plus(new BigNumber(hours).times(60 * 60))
+    .plus(new BigNumber(day).times(24 * 60 * 60));
+
+  return allSeconds.toNumber();
 }
